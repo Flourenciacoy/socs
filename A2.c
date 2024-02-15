462d6eff03257ef07138a38c7e07d0f29e124b93
Flourenciacoy <149255869+Flourenciacoy@users.noreply.github.com>
2024-02-15 11:44:52 +0700

    Add files via upload

---
+++ A2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B, hasil;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	hasil= A+B;
+	printf("%d\n", hasil);
+	
+	return 0;
+}
---
+++ A3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int A;
+	int ganjil;
+	int genap;
+	
+	scanf("%d", &A);
+	
+	if (A % 2 == 0) 
+	{
+		printf("EVEN\n", genap);
+	}
+    else
+    {
+    	printf("ODD\n", ganjil);
+	}
+	
+	return 0;
+}
